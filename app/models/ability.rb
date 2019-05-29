b5f225ce25626154d8eb393acce60f20f1372dd2
beskhlebnyi <beskhlebnyi@gmail.com>
2019-05-29 14:40:12 +0300

    add rake task for permissions creating

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,17 +4,9 @@ class Ability
   def initialize(admin)
     admin.role.permissions.each do |permission|
       if permission.subject_class == "all"
-        if permission.name = "can"
-          can permission.action.to_sym, permission.subject_class.to_sym  
-        elsif permission.name = "cannot"
-          cannot permission.action.to_sym, permission.subject_class.to_sym
-        end  
+        can permission.action.to_sym, permission.subject_class.to_sym  
       else
-        if permission.name = 'can'
-          can permission.action.to_sym, permission.subject_class.constantize 
-        elsif  permission.name = 'cannot'
-          cannot permission.action.to_sym, permission.subject_class.constantize
-        end
+        can permission.action.to_sym, permission.subject_class.constantize 
       end
     end
   end
